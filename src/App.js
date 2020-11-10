6296f5ecc3c4bd80d976b778b27989e2caac60eb
JeranDev <jeranjb@gmail.com>
2020-11-10 10:25:52 -0700

    add nav component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 //Add Style
 import './styles/app.min.css'
 //Add Components
 import Player from './components/Player'
 import Song from './components/Song'
 import Library from './components/Library'
+import Nav from './components/Nav'
 //Add Util
 import data from './util'
 
 function App() {
+  //Ref
+  const audioRef = useRef(null)
   //State
   const [songs, setSongs] = useState(data())
   const [currentSong, setCurrentSong] = useState(songs[0])
   const [isPlaying, setIsPlaying] = useState(false)
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  })
+  const [libraryStatus, setLibraryStatus] = useState(false)
+  const timeUpdateHandler = e => {
+    const current = e.target.currentTime
+    const duration = e.target.duration
+    setSongInfo({ ...songInfo, currentTime: current, duration })
+  }
+
   return (
     <div className='App'>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
+        songInfo={songInfo}
+        setSongInfo={setSongInfo}
+        audioRef={audioRef}
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
       />
-      <Library songs={songs} setCurrentSong={setCurrentSong} />
+      <Library
+        libraryStatus={libraryStatus}
+        setSongs={setSongs}
+        isPlaying={isPlaying}
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   )
 }
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import LibrarySong from './LibrarySong'
 
-const Library = ({ songs, setCurrentSong }) => {
+const Library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
   return (
-    <div className='library'>
+    <div className={`library ${libraryStatus ? 'active-library' : ''}`}>
       <h2>Library</h2>
       <div className='library-songs'>
         {songs.map(song => (
           <LibrarySong
+            setSongs={setSongs}
+            isPlaying={isPlaying}
             songs={songs}
             song={song}
             setCurrentSong={setCurrentSong}
             id={song.id}
             key={song.id}
+            audioRef={audioRef}
           />
         ))}
       </div>
